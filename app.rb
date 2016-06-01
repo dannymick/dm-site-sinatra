795f4a3bc0e05376a419ad131d92fb770a8a2620
Danny <dannymickleburgh@gmail.com>
2016-06-01 19:33:07 -0400

    clean up app.rb file

--- app.rb
+++ app.rb
@@ -10,7 +10,6 @@ class App < Sinatra::Base
 	set :views, File.dirname(__FILE__) + '/views'
 	set :public_folder, 'public'
 
-	# @@page_data = YAML.load_file('data.yaml')
 	file = File.read('data.json')
 	@@page_data = JSON.parse(file)
 
@@ -29,13 +28,7 @@ class App < Sinatra::Base
 				@project = work
 			end
 		end
-		puts @project
+
 		erb :project, :locals => {:project => @project }
-		# erb :project, :locals => {:page_data => @@page_data }
 	end
 end
-
-# project_url_array = ["fplwatch", "slader", "sierra-mist", "tylenol", "mry", "sumday", lurkers", "jade-hotel"]
-
-# tylenol:
-# America's hangover cure, Tylenol, reached out to MRY for a hip new responsive website with a Drupal 7 backend to replace their old, ugly flash website. Over the course of six weeks I worked on this project with two other developers, all three of us had no prior knowledge of Drupal or Drupal theming.
